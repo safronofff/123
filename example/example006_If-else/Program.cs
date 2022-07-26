49270633f5bc6fa2a7ff5526b6e630b8e52c0bbe
ys-off <yuriy.safronoff@yandex.ru>
2022-07-26 20:36:17 +0300

    example006-IfElse

---
+++ example/example006_If-else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "аня")
+{
+	Console.WriteLine("Привет Анютка!Как твои дела?!");
+}
+else
+{
+	Console.Write("Привет!");
+	Console.WriteLine(username);
+}
